3b4d1dc63cc4f0cb129cce7d22525728ddd42c1e
ThangBeooooooo <2151010348thang@ou.edu.vn>
2024-08-15 23:41:01 +0700

    update OrderStatistics

--- app/src/main/java/com/hfing/shoesstore/Adapter/OrderAdapter.java
+++ app/src/main/java/com/hfing/shoesstore/Adapter/OrderAdapter.java
@@ -1,3 +1,144 @@
+//package com.hfing.shoesstore.Adapter;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.TextView;
+//
+//import com.hfing.shoesstore.Model.Orders;
+//import com.hfing.shoesstore.R;
+//
+//import java.util.List;
+//
+//public class OrderAdapter extends BaseAdapter {
+//
+//    private final List<Orders> orders;
+//    private final Context context;
+//
+//    public OrderAdapter(List<Orders> orders, Context context) {
+//        this.orders = orders;
+//        this.context = context;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return orders.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        return orders.get(position);
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return orders.get(position).getId();
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        if (convertView == null) {
+//            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//            convertView = inflater.inflate(R.layout.order_list_item, parent, false);
+//        }
+//
+//        Orders order = orders.get(position);
+//        TextView tvOrderId = convertView.findViewById(R.id.tvOrderId);
+//        TextView tvUserId = convertView.findViewById(R.id.tvUserId);
+//        TextView tvProductName = convertView.findViewById(R.id.tvProductName);
+//        TextView tvProductPrice = convertView.findViewById(R.id.tvProductPrice);
+//
+//        tvOrderId.setText(String.valueOf(order.getId()));
+//        tvUserId.setText(String.valueOf(order.getUser_id()));
+//        tvProductName.setText(order.getProductName());
+//        tvProductPrice.setText(String.valueOf(order.getUnitPrice()));
+//
+//        return convertView;
+//    }
+//}
+
+//package com.hfing.shoesstore.Adapter;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.TextView;
+//
+//import com.hfing.shoesstore.DAO.OrderDAO;
+//import com.hfing.shoesstore.DAO.OrderDetailDAO;
+//import com.hfing.shoesstore.DAO.ProductDAO;
+//import com.hfing.shoesstore.DAO.ProductSizeDAO;
+//import com.hfing.shoesstore.Model.OrderDetail;
+//import com.hfing.shoesstore.Model.Orders;
+//import com.hfing.shoesstore.Model.Product;
+//import com.hfing.shoesstore.R;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class OrderAdapter extends BaseAdapter {
+//
+//    private final List<OrderDetail> orderDetails;
+//    private final Context context;
+//    private final ProductDAO productDAO;
+//    private final OrderDAO orderDAO;
+//    private final ProductSizeDAO productSizeDAO;
+//
+//    public OrderAdapter(List<OrderDetail> orderDetails, Context context) {
+//        this.orderDetails = orderDetails;
+//        this.context = context;
+//        productDAO = new ProductDAO(context);
+//        orderDAO = new OrderDAO(context);
+//        productSizeDAO = new ProductSizeDAO(context);
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return orderDetails.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        return orderDetails.get(position);
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return orderDetails.get(position).getId();
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        if (convertView == null) {
+//            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//            convertView = inflater.inflate(R.layout.order_list_item, parent, false);
+//        }
+//
+//        OrderDetail orderDetail = orderDetails.get(position);
+//        TextView tvOrderId = convertView.findViewById(R.id.tvOrderId);
+//        TextView tvProductId = convertView.findViewById(R.id.tvProductId);
+//        TextView tvProductSizeId = convertView.findViewById(R.id.tvProductSizeId);
+//        TextView tvQuantity = convertView.findViewById(R.id.tvQuantity);
+//        TextView tvUnitPrice = convertView.findViewById(R.id.tvUnitPrice);
+//        TextView tvDate = convertView.findViewById(R.id.tvDate);
+//
+//        tvOrderId.setText(String.valueOf(orderDetail.getOrder_id()));
+//        Product product = productDAO.getProductById(orderDetail.getProduct_id());
+//        tvProductId.setText(product.getName());
+//        tvProductSizeId.setText(String.valueOf(orderDetail.getProduct_size_id()));
+//        tvQuantity.setText(String.valueOf(orderDetail.getQuantity()));
+//        tvUnitPrice.setText(String.valueOf(orderDetail.getUnit_price()));
+//        Orders order = orderDAO.getOrderById(orderDetail.getOrder_id());
+//        tvDate.setText(order.getOrder_date());
+//
+//        return convertView;
+//    }
+//}
+
 package com.hfing.shoesstore.Adapter;
 
 import android.content.Context;
@@ -7,34 +148,46 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.hfing.shoesstore.DAO.OrderDAO;
+import com.hfing.shoesstore.DAO.ProductDAO;
+import com.hfing.shoesstore.DAO.ProductSizeDAO;
+import com.hfing.shoesstore.Model.OrderDetail;
 import com.hfing.shoesstore.Model.Orders;
+import com.hfing.shoesstore.Model.Product;
+import com.hfing.shoesstore.Model.ProductSize;
 import com.hfing.shoesstore.R;
 
 import java.util.List;
 
 public class OrderAdapter extends BaseAdapter {
 
-    private final List<Orders> orders;
+    private final List<OrderDetail> orderDetails;
     private final Context context;
+    private final ProductDAO productDAO;
+    private final OrderDAO orderDAO;
+    private final ProductSizeDAO productSizeDAO;
 
-    public OrderAdapter(List<Orders> orders, Context context) {
-        this.orders = orders;
+    public OrderAdapter(List<OrderDetail> orderDetails, Context context) {
+        this.orderDetails = orderDetails;
         this.context = context;
+        productDAO = new ProductDAO(context);
+        orderDAO = new OrderDAO(context);
+        productSizeDAO = new ProductSizeDAO(context);
     }
 
     @Override
     public int getCount() {
-        return orders.size();
+        return orderDetails.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return orders.get(position);
+        return orderDetails.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return orders.get(position).getId();
+        return orderDetails.get(position).getId();
     }
 
     @Override
@@ -44,16 +197,23 @@ public class OrderAdapter extends BaseAdapter {
             convertView = inflater.inflate(R.layout.order_list_item, parent, false);
         }
 
-        Orders order = orders.get(position);
+        OrderDetail orderDetail = orderDetails.get(position);
         TextView tvOrderId = convertView.findViewById(R.id.tvOrderId);
-        TextView tvUserId = convertView.findViewById(R.id.tvUserId);
-        TextView tvProductName = convertView.findViewById(R.id.tvProductName);
-        TextView tvProductPrice = convertView.findViewById(R.id.tvProductPrice);
+        TextView tvProductId = convertView.findViewById(R.id.tvProductId);
+        TextView tvProductSizeId = convertView.findViewById(R.id.tvProductSizeId);
+        TextView tvQuantity = convertView.findViewById(R.id.tvQuantity);
+        TextView tvUnitPrice = convertView.findViewById(R.id.tvUnitPrice);
+        TextView tvDate = convertView.findViewById(R.id.tvDate);
 
-        tvOrderId.setText(String.valueOf(order.getId()));
-        tvUserId.setText(String.valueOf(order.getUser_id()));
-        tvProductName.setText(order.getProductName());
-        tvProductPrice.setText(String.valueOf(order.getUnitPrice()));
+        tvOrderId.setText(String.valueOf(orderDetail.getOrder_id()));
+        Product product = productDAO.getProductById(orderDetail.getProduct_id());
+        tvProductId.setText(product.getName());
+        ProductSize productSize = productSizeDAO.getProductSizeById(orderDetail.getProduct_size_id());
+        tvProductSizeId.setText(productSize.getSize());
+        tvQuantity.setText(String.valueOf(orderDetail.getQuantity()));
+        tvUnitPrice.setText(String.valueOf(orderDetail.getUnit_price()));
+        Orders order = orderDAO.getOrderById(orderDetail.getOrder_id());
+        tvDate.setText(order.getOrder_date());
 
         return convertView;
     }
